5d5c31b0fe32de6c138a5f3f065cca4ee03740f7
Erkan <rk4an@users.noreply.github.com>
2016-09-23 10:56:31 +0200

    Update WeatherMapDataSource_centreon.php

--- lib/datasources/WeatherMapDataSource_centreon.php
+++ lib/datasources/WeatherMapDataSource_centreon.php
@@ -6,9 +6,15 @@
 
 class WeatherMapDataSource_centreon extends WeatherMapDataSource {
 
+        var $server = "localhost";
+        var $database = "centreon_storage";
+        var $login = "root";
+        var $password = "";
+
         var $host = "";
         var $service = "";
         var $results = array();
+
 
         function Init(&$map)
         {
@@ -35,23 +41,25 @@ class WeatherMapDataSource_centreon extends WeatherMapDataSource {
 
         function ReadData($targetstring, &$map, &$item)
         {
-            $mysqli = new mysqli("localhost", "root", "", "centreon_storage");
-            
-            $result = $mysqli->query('
-            select *
-            from metrics as m,
-            index_data as i
-            where m.index_id = i.id
-            and i.host_name like "%'.$this->host.'%"
-            and i.service_description like "%'.$this->service.'%"');
-            
-            $value = array();
-            while($row = mysqli_fetch_object($result)) {
-              $value[$row->metric_name] = $row->current_value;
-            }
-            
-            $data_time = 0;
-            return( array($value["traffic_in"], $value["traffic_out"], $data_time) );
+
+                $mysqli = new mysqli($this->server, $this->login, $this->password, $this->database);
+
+                $result = $mysqli->query('
+                select *
+                from metrics as m,
+                index_data as i
+                where m.index_id = i.id
+                and i.host_name like "%'.$this->host.'%"
+                and i.service_description like "%'.$this->service.'%"');
+                
+                $value = array();
+                while($row = mysqli_fetch_object($result)) {
+                  $value[$row->metric_name] = $row->current_value;
+                }
+
+                $data_time = 0;
+                return( array($value["traffic_in"], $value["traffic_out"], $data_time) );
+
         }
 }
 
